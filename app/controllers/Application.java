7afaa43b5d544342e02b470deb7de6613b9dd4f4
azat <azat.vlv@gmail.com>
2015-11-25 09:56:24 +0300

    Initial commit

---
+++ app/controllers/Application.java
@@ -0,0 +1,64 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class Application extends Controller {
+
+    public static void index() {
+        render();
+    }
+
+    public static void d3a() {
+        render();
+    }
+    public static void d3b() {
+        render();
+    }
+    public static void d3c() {
+        render();
+    }
+    public static void d3d() {
+        render();
+    }
+    public static void d3e() {
+        render();
+    }
+
+
+    public static void chartjs_a() {
+        render();
+    }
+    public static void chartjs_b() {
+        render();
+    }
+    public static void chartjs_c() {
+        render();
+    }
+
+    public static void sigma() {
+        render();
+    }
+
+    public static void highcharts_a() {
+        render();
+    }
+    public static void highcharts_b() {
+        render();
+    }
+    public static void highcharts_c() {
+        render();
+    }
+    public static void highcharts_d() {
+        render();
+    }
+    public static void highcharts_e() {
+        render();
+    }
+    public static void highcharts_f() {
+        render();
+    }
+
+    public static void timesheet() {
+        render();
+    }
+}
